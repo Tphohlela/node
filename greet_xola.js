bf4f0991d8ae8426062b2308b483d1a37b095126
Thato <thatophohlela.com>
2021-07-19 13:26:22 +0200

    second commit

--- greet_xola.js
+++ greet_xola.js
@@ -1,15 +1,7 @@
-// //import the greet module that is in the current folder
-// const greet = require('./greet');
 
-// console.log(greet('Xola'));
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
 
-var figlet = require('figlet');
- 
-figlet('Hello World!!', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
-});
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
